1524ed99966cf81acb09459be9ed427bb122f80c
kuliahinformatika <murodalhakim01@gmail.com>
2025-01-05 10:24:58 +0700

    menambah percabangan

--- main.py
+++ main.py
@@ -19,3 +19,24 @@ print('ibu berkata "Pergi ke toko"')
 print('budi menajawab,"ok ma, apa yang saya lakukan ditoko?"')
 print('ibu berkata "belikan telur"')
 print("oke ma", 'budi pergi ke toko')
+
+#pecabangan
+Jumlah_susu = 234
+Jumlah_telur = 6
+
+print(f"Jumlah botol susu adalah {Jumlah_susu} botol")
+print(f"Jumlah telur adalah {Jumlah_telur} botol")
+
+if Jumlah_susu > 0:
+    print("Budi mengecek harganya, uangnya cukup")
+    print("Budi membawa 1 botol susu")
+    if Jumlah_telur > 0:
+        print("Budi membeli 6 butir telur")
+    else:
+        print("Budi membeli 1 butir telur")
+
+else:
+    print("Budi tidak membeli susu")
+
+print ("Budi pulang ke rumah")
+print ("Budi menyerahkan hasil ke ibunya")
