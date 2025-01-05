f780092ae3451f2c6687cd0731fbb900aca8fb83
kuliahinformatika <murodalhakim01@gmail.com>
2025-01-05 09:22:18 +0700

    menjelaskan squensial

--- main.py
+++ main.py
@@ -2,6 +2,20 @@
 ini adalah demo project pertama dengan python
 """
 
-print("Hello World")
-print("My name is Hakim")
+# print("Hello World")
+# print("My name is Hakim")
+# print ("5 januari 2025")
+# print("Python RWID")
 
+"""
+Semua sintaks dasar bahasa pemograan terdiri dari :
+1. Sekuensial : langkah berurutan
+2. Percabangan : langkah melompat jika kondisi terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi 
+"""
+#squensial
+
+print('ibu berkata "Pergi ke toko"')
+print('budi menajawab,"ok ma, apa yang saya lakukan ditoko?"')
+print('ibu berkata "belikan telur"')
+print("oke ma", 'budi pergi ke toko')
