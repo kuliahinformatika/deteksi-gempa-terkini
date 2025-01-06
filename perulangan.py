8ff6722b7f91013c5285b44cb2f79697893020a5
kuliahinformatika <murodalhakim01@gmail.com>
2025-01-06 08:17:00 +0700

    menambah perulangan.py

---
+++ perulangan.py
@@ -0,0 +1,9 @@
+Jumlah_buku = 10
+print('ibu berkata "baca bukumu"')
+buku_yang_dibaca = 5
+print("Jumlah buku yang dibaca")
+
+for buku_yang_dibaca in range (1, Jumlah_buku + 1):
+    print(f"buku yang dibaca {buku_yang_dibaca} sudah dibaca")
+    
+print(buku_yang_dibaca)
