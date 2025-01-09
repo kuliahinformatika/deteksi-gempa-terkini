7f3a2440b14006bce3a1299c1ce049e862a14db0
kuliahinformatika <murodalhakim01@gmail.com>
2025-01-09 09:36:18 +0700

    update while_undifined

--- while_undifined.py
+++ while_undifined.py
@@ -1,18 +1,18 @@
 """
 Program perulangan membaca buku dengan while
 """
-Jumlah_buku = 10
-total_jumlah_baca = 0
-Jumlah_buku_sudah_dibaca_dan_dipahami = 0
+buku = 10
+Jumlah_baca = 0
+Jumlah_dipahami = 0
 
-while total_jumlah_baca < Jumlah_buku * 1:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if Jumlah_buku_sudah_dibaca_dan_dipahami == 9:
-        print(f"Buku ke {Jumlah_buku_sudah_dibaca_dan_dipahami +1} belum paham")
+while Jumlah_baca < buku * 2:
+    Jumlah_baca = Jumlah_baca + 1
+    if Jumlah_dipahami == 9:
+        print(f"Buku ke {Jumlah_dipahami +1} belum paham")
     else:
-        Jumlah_buku_sudah_dibaca_dan_dipahami = Jumlah_buku_sudah_dibaca_dan_dipahami + 1
-        print(f"Buku ke {Jumlah_buku_sudah_dibaca_dan_dipahami} sudah paham")
+        Jumlah_dipahami = Jumlah_dipahami + 1
+        print(f"Buku ke {Jumlah_dipahami} sudah paham")
     
 
-print(f"Jumlah Buku yang sudah di baca {Jumlah_buku_sudah_dibaca_dan_dipahami} buah")
+print(f"Jumlah Buku yang sudah di baca {Jumlah_dipahami} buah")
 #while digunakan perulangan yang tidak tertentu sampai kondisi true
