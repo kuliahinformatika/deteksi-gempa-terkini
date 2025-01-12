cdea327ee7683a0efd44bee08bec87e8ae640272
kuliahinformatika <murodalhakim01@gmail.com>
2025-01-12 11:12:29 +0700

    merubah dictionary ke json

--- dictionary.py
+++ dictionary.py
@@ -8,3 +8,13 @@ users = {
 print(users)
 print(users['id'])
 print(users['name'])
+print(type(users))
+
+print("\nubah dictionary ke json")
+import json
+result = json.dumps(users)
+print(result)
+print(type(result))
+
+with open('users.json', 'w') as file:
+    json.dump(users, file)#dump tanpa s menghasilkan file
